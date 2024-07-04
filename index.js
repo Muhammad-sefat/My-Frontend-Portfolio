c905eb9efe8a000e9e992338978e8654f88df801
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-07-04 10:05:39 +0600

    Complete Responsive for all device

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// toggle icon
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
 // Scrollbar
 let sections = document.querySelectorAll("section");
 let navlinks = document.querySelectorAll("header nav a");
@@ -18,7 +27,29 @@ window.onscroll = () => {
       });
     }
   });
+
   //   create sticky navbar
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 100);
+
+  //   remove toggle icon
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
+
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-image,.services-container,.portfolio-box,.contact form",
+  {
+    origin: "bottom",
+  }
+);
+ScrollReveal().reveal(".home-content h1, .about-image", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
